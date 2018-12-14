535e67a4ee2640e7572ea0401ac0af484878ef3c
Peter Heatwole <pheatwole@gmail.com>
2018-12-14 15:44:17 -0800

    Change to MathJax for HTML equations

--- conf.py
+++ conf.py
@@ -32,7 +32,8 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.imgmath',
+    # 'sphinx.ext.imgmath',
+    'sphinx.ext.mathjax',
     'sphinx.ext.todo',
     'sphinxcontrib.bibtex']
 
