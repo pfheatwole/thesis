58167937ea6da8a43c171aaa6bfc3f7ef853599a
Peter Heatwole <pheatwole@gmail.com>
2019-11-07 09:54:31 -0800

    Reduce body font to 10pt

--- conf.py
+++ conf.py
@@ -139,7 +139,7 @@ latex_elements = {
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    'pointsize': '11pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
