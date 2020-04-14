5da4b01b6291c1f676050e92b3f86aff9e847b2d
Peter Heatwole <pheatwole@gmail.com>
2020-04-13 17:17:22 -0700

    Add a logo

--- source/conf.py
+++ source/conf.py
@@ -106,7 +106,9 @@ html_title = project  # Override "<project>'s documentation"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo': 'hook3_freecad_clear.png',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
