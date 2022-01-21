0bb1a31be2b22ddac5ef1826514bb36a411bd6d5
Peter Heatwole <pfheatwole@gmail.com>
2022-01-21 08:56:12 -0800

    Switch Sphinx theme to Furo

--- source/conf.py
+++ source/conf.py
@@ -135,40 +135,13 @@ numfig = True  # Enable numbered figure and table references in HTML
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_title = project  # Override "<project>'s documentation"
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {
-    'fixed_sidebar': True,
-    'logo': 'hook3_vectorized_opt.svg',
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
-    ]
-}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
